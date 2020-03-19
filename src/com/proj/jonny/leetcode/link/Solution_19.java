f1c3d8698c31a434fe7a4750e7332592a50298ba
jonnyliu <980463316@qq.com>
2020-03-19 12:59:59 +0800

    刷题第7天：删除链表第N个节点

---
+++ src/com/proj/jonny/leetcode/link/Solution_19.java
@@ -0,0 +1,82 @@
+package com.proj.jonny.leetcode.link;
+
+import java.util.StringJoiner;
+
+/**
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ * <p>
+ * 示例：
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * <p>
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * <p>
+ * 说明：
+ * <p>
+ * 给定的 n 保证是有效的。
+ * <p>
+ * 进阶：
+ * <p>
+ * 你能尝试使用一趟扫描实现吗？
+ */
+public class Solution_19 {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode forth = new ListNode(4);
+        ListNode fifth = new ListNode(5);
+        head.next = second;
+//        second.next = third;
+//        third.next = forth;
+//        forth.next = fifth;
+
+        System.out.println(removeNthFromEnd(head, 2));
+    }
+
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+
+        ListNode slow = dummyHead;
+        ListNode fast = dummyHead;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        System.out.println(fast);
+        System.out.println(slow);
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        ListNode delNode = slow.next;
+        slow.next = delNode.next;
+        delNode.next = null;
+
+        return dummyHead.next;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return new StringJoiner(", ", ListNode.class.getSimpleName() + "[", "]")
+                    .add("val=" + val)
+                    .add("next=" + next)
+                    .toString();
+        }
+    }
+
+}
