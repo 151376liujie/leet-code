b8191582684479512e137a22e9a57160a6916a4e
liujie <liujie361@jd.com>
2020-03-31 12:42:50 +0800

    刷题第13天：验证二分搜索树

---
+++ src/com/proj/jonny/leetcode/bst/Solution_98.java
@@ -0,0 +1,56 @@
+package com.proj.jonny.leetcode.bst;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * <p>
+ * 假设一个二叉搜索树具有如下特征：
+ * <p>
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 示例 1:
+ * <p>
+ * 输入:
+ * 2
+ * / \
+ * 1   3
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:
+ * 5
+ * / \
+ * 1   4
+ *      / \
+ *     3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ *      根节点的值为 5 ，但是其右子节点值为 4 。
+ * <p>
+ */
+public class Solution_98 {
+
+    public boolean isValidBST(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).compareTo(list.get(i + 1)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void inOrder(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left, list);
+        list.add(node.val);
+        inOrder(node.right, list);
+    }
+
+}
