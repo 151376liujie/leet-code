500370db20ced7cc4e3991cce10d94e0133eef09
jonnyliu <980463316@qq.com>
2020-04-18 13:00:16 +0800

    刷题第22天：617.合并二叉树

---
+++ src/com/proj/jonny/leetcode/tree/Solution_617.java
@@ -0,0 +1,56 @@
+package com.proj.jonny.leetcode.tree;
+
+/**
+ * 617.合并二叉树
+ * <p>
+ * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+ * <p>
+ * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:
+ * Tree 1                     Tree 2
+ * 1                         2
+ * / \                       / \
+ * 3   2                     1   3
+ * /                           \   \
+ * 5                             4   7
+ * 输出:
+ * 合并后的树:
+ * 3
+ * / \
+ * 4   5
+ * / \   \
+ * 5   4   7
+ * 注意: 合并必须从两个树的根节点开始。
+ * <p>
+ * Author: jonny
+ * Time: 2020-04-18 12:56.
+ */
+public class Solution_617 {
+
+
+    public static void main(String[] args) {
+        TreeNode t1 = TreeNodeUtils.buildFrom(1, 3, 2, 5);
+        TreeNode t2 = TreeNodeUtils.buildFrom(2, 1, 3, null, 4, null, 7);
+        TreeNode node = mergeTrees(t1, t2);
+        System.out.println(node);
+    }
+
+    public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+
+        TreeNode root = new TreeNode(t1.val + t2.val);
+        root.left = mergeTrees(t1.left, t2.left);
+        root.right = mergeTrees(t1.right, t2.right);
+        return root;
+    }
+
+
+}
