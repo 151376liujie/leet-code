4636798afb1312aa201963fb6867284de6e31990
liujie <980463316@qq.com>
2020-04-30 17:44:49 +0800

    排序算法练习-选择排序

--- src/com/proj/jonny/leetcode/array/Sorts.java
+++ src/com/proj/jonny/leetcode/array/Sorts.java
@@ -9,12 +9,27 @@ public class Sorts {
 
 
     public static void main(String[] args) {
-        int[] array = new int[]{9, 4, 5, 6, 1, 2, 3};
+        Integer[] array = new Integer[]{9, 4, 2, 5, 6, 1, 3};
         System.out.println("before sort: " + Arrays.toString(array));
+        selectionSort(array);
+        System.out.println("after selection sort: " + Arrays.toString(array));
 //        bubbleSort(array);
 //        System.out.println("after bubble sort: " + Arrays.toString(array));
-        insertionSort(array);
-        System.out.println("after insertion sort: " + Arrays.toString(array));
+//        insertionSort(array);
+//        System.out.println("after insertion sort: " + Arrays.toString(array));
+
+//        for (int i = 0; i < 1000; i++) {
+//            List<Integer> list = new ArrayList<>();
+//            IntStream.range(1, 50).forEach(idx -> list.add(new Random().nextInt(100)));
+//            Integer[] integers = list.toArray(new Integer[0]);
+//            selectionSort(integers);
+//            System.out.println("after selection sort: " + Arrays.toString(integers));
+//            for (int i1 = 0; i1 < integers.length - 1; i1++) {
+//                if (integers[i1] > integers[i1 + 1]) {
+//                    throw new IllegalStateException();
+//                }
+//            }
+//        }
     }
 
     private static void bubbleSort(int[] arr) {
@@ -58,24 +73,33 @@ public class Sorts {
         }
     }
 
-
-    // 插入排序，a表示数组，n表示数组大小
-    public static void insertionSort2(int[] arr) {
-        if (arr.length <= 1) return;
-
-        for (int i = 1; i < arr.length; ++i) {
-            int value = arr[i];
+    /**
+     * 思路：将数组分为两个区，待排序区和已排序区，
+     * 每次从待排序区中找出最小值，然后跟已排序区元素比较
+     * 如果待排序区的最小值小于已排序区元素，交换位置
+     *
+     * @param arr
+     */
+    private static void selectionSort(Integer[] arr) {
+        for (int i = 1; i < arr.length; i++) {
             int j = i - 1;
-            // 查找插入的位置
-            for (; j >= 0; --j) {
-                if (arr[j] > value) {
-                    arr[j + 1] = arr[j];  // 数据移动
-                } else {
-                    break;
-                }
+            int indexOfMin = min(arr, i);
+            if (arr[indexOfMin] < arr[j]) {
+                int tmp = arr[j];
+                arr[j] = arr[indexOfMin];
+                arr[indexOfMin] = tmp;
             }
-            arr[j + 1] = value; // 插入数据
         }
+    }
+
+    private static int min(Integer[] arr, int startIndex) {
+        int min = startIndex;
+        for (int i = startIndex + 1; i < arr.length; i++) {
+            if (arr[min] > arr[i]) {
+                min = i;
+            }
+        }
+        return min;
     }
 
 }
